c93d945e79474b9d902d0b0f5d1d5523e20e9750
Elijah Gachuhi <elgachuhi@gmail.com>
2015-06-09 10:15:44 -0400

    import files from chrome-nfc

--- photo_hex_util.py
+++
@@ -1,5 +0,0 @@
-import binascii
-filename = 'photo.jpg'
-with open(filename, 'rb') as f:
-    content = f.read()
-print(binascii.hexlify(content))
--- write.py
+++
@@ -1,6 +0,0 @@
-clf = nfc.ContactlessFrontend('usb')
-tag = clf.connect(rdwr={'on-connect':None})
-sp = nfc.ndef.SmartPosterRecord('htttp://www.egachuhi.me')
-sp.title = "His Awesomeness"
-tag.ndef.message = nfc.ndef.Message(sp)
-print nfc.ndef.SmartPosterRecord(tag.ndef.message[0].pretty())
