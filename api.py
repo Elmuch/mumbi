768932ddff13e4ee31b616629967982f6a1c3fa0
Elijah Gachuhi <elgachuhi@gmail.com>
2015-06-09 13:53:13 -0400

    write 20 ndef record into mifare-desfire

--- api.py
+++ api.py
@@ -14,6 +14,7 @@ clf = nfc.ContactlessFrontend('usb')
 @app.route('/card-api/read')
 def read():
 	tag = clf.connect(rdwr={'on-connect': None})
+	print tag.ndef.message.pretty()
 	return str(tag.ndef.message)
 
 filename = 'photo.jpg'
@@ -27,10 +28,8 @@ def write():
 	tag = clf.connect(rdwr={'on-connect': None})
 	data = request.json
 	ndef_records = []
-
-	for objs in data:
-		for key in objs:
-			ndef_records.append(nfc.ndef.Record("urn:nfc:wkt:T", key,objs[key]))
+	for key in data:
+		ndef_records.append(nfc.ndef.Record("urn:nfc:wkt:T", key,str(data[key])))
 
 	tag.ndef.message = nfc.ndef.Message(ndef_records)
 
@@ -50,3 +49,5 @@ if __name__ == "__main__":
     host="0.0.0.0",
     port=int("5000")
   )
+# write()
+# read()
--- demo_client/ui.js
+++ demo_client/ui.js
@@ -15,15 +15,35 @@
 	});
 
 	$('form').submit(function(event) {
+		var sample_data = {
+			"tra_id":1,
+		  "tra_pro_id":1,
+		  "tra_ter_id":1,
+		  "tra_sta_id":15,
+		  "tra_ins_id":2,
+		  "tra_geo4_id":158,
+		  "tra_trarepenr_id":1506,
+		  "tra_houmemenr_id":14266,
+		  "tra_haschangedgeolocation":0,
+		  "tra_number":1,
+		  "tra_accountnumber":"",
+		  "tra_flagcollected":1,
+		  "tra_consecutiveunclaimed":0,
+		  "tra_valuesubsidy":10000.0,
+		  "tra_valuearrears":0.0,
+		  "tra_valuearrearsenrolment":0.0,
+		  "tra_valuetotal":10000.0,
+		  "tra_istransferfieldwork":0,
+		  "tra_datefieldwork":"",
+		  "tra_fieldwork_use_id":0
+		}
+		
 		var req = $.ajax({ url: 'http://127.0.0.1:5000/card-api/write',
-							type: "POST",
-							contentType: 'application/json',
-						 		data: JSON.stringify([{
-              	name: 'elijah',
-              	id: '343434'
-            		}]),
-            		dataType: 'json'
-							});
+			type: "POST",
+			contentType: 'application/json',
+		 		data: JSON.stringify(sample_data),
+	  		dataType: 'json'
+		});
 
 
 		req.done(function(data){
